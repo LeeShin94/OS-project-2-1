e2e4eaa20b70939eff1235e2845f68271c8506d4
LeeShin94 <thesameli1126@gmail.com>
2017-11-16 10:54:52 -0500

    modified

--- isReadable.c
+++ isReadable.c
@@ -3,18 +3,16 @@
 #include <linux/sched.h>
 #include <linux/mm.h>
 
-asmlinkage long sys_isReadable(void *p){
-	unsigned long ptr = (unsigned long) p;
-	struct vm_area_struct *vma = find_vma(get_current()->mm, ptr);
-	
-	if((vma->vm_flags & VM_READ) == VM_READ){
-		printk("READ");
-		return 0;	
+asmlinkage long sys_isReadable(char* p){
+	struct vm_area_struct *vma = find_vma(get_current()->mm, (unsigned long)p);
+	if ((vma->vm_flags & VM_READ) == VM_READ){
+		printk("READ");	
+		return 0;
 	}
-	if((vma->vm_flags & VM_EXEC) == VM_EXEC){
-		printk("EXEC");
-		return 0;	
+	if ((vma->vm_flags & VM_EXEC) == VM_EXEC){
+		printk("EXEC");	
+		return 0;
 	}
-	printk("No safe");
+	printk("No safe reading");
 	return -1;
 }
--- main.c
+++ main.c
@@ -5,29 +5,27 @@
 #include <syscall.h>
 
 int readAddr(void *p, int a){
-    long int result = syscall(318, p);
-    //printf("system call returned %ld\n", result);
+    long int result = syscall(317, p);
     if (result == 0){
         a = p;
-        printf("%ld\n",a);
+        printf("%p,",p);
+        printf("%d\n",a);
     }
 }
 
 int main()
 {
     unsigned long PAGE_SIZE = 0, TASK_SIZE = 0;
-
     PAGE_SIZE = sysconf(_SC_PAGESIZE);
-    if(sizeof(void*) == sizeof(int))// 32 bit
+    if(sizeof(void*) == sizeof(int))
         TASK_SIZE = 0xc0000000UL;
-    else //64 bit
+    else
         TASK_SIZE = (1UL << 47) - PAGE_SIZE;
 
     char *p = 0;
-    for(;p< TASK_SIZE;p+= PAGE_SIZE*1024){
+    for(;p<TASK_SIZE;p+= PAGE_SIZE*1024){
         int a = 0;
         readAddr(p, &a);
     }
-
     return 0;
 }
